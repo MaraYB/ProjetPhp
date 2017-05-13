4da19d409b8696a147d50c92be8de1e58d5215d4
LasJu <julius.saleille@etu.univ-lille3.fr>
2017-05-13 18:10:19 +0200

    Ajouter un bar à la base
    entreebar.php donne la page sur laquelle l'utilisateur écrira les
    informations du bar qu'il souhaite ajouter.
    
    ajoutbar.php permet d'ajouter les informations du formulaire que
    l'utilisateur a rempli sur la page entreebar.php dans la base de données
    basebar.db

---
+++ entreebar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" href="style.css">
+		<title>Ajout Bar</title>
+	</head>
+	<body>
+		<h1>Contribuez au site en ajoutant votre bar favori</h1>
+		<form action="ajoutbar.php" method="POST">
+			Nom du bar: <input type="text" name="nom_bar"/><br/>
+			Adresse: <input type="text" name="adresse_bar"/><br/>
+			Code Postal: <input type="text" name="cp_ville"/><br/>
+			Ville: <select name="ville">
+				<?php
+					$db= new SQLite3('basebar.db');
+					$resultats=$db ->query('SELECT DISTINCT "ville" FROM databar ORDER BY "ville"');
+					while ($row = $resultats -> fetchArray()){
+						if ($row[0]){
+							echo"<option value='",$row[0],"'>",$row[0], "</option> \n ";
+						}
+					}
+				?>
+				</select><br/>
+			Département: <input type="text" name="departement"/><br/>
+			Spécialités: <input type="text" name="specialite_bar"/><br/>
+			<input type="submit" name="submit" value="Ajouter"/>
+		</form>
+	</body>
+</html>
